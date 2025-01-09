6bb43a562385ba4b682341cca8014a5baded06d6
Leonardo Silva <raimundo.silva@icc.org.br>
2025-01-09 19:52:20 -0300

    Primeiro commit do projeto Organo React

---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import './Banner.css'
+function Banner(){
+    return(
+        <header className="banner">
+        <img src="/imagens/logo-digital.png" alt="o banner principal do Organo" />
+        </header>
+    )
+}
+export default Banner
